b8277f7d023978acc80394aeccd9f2ccd2cba42d
Tim Keiner <keiner.tim@gmail.com>
2023-08-21 23:41:14 +0200

    add files from play.tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        yellow: {
+          bvb: "yellow",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
